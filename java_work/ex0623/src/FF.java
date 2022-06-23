55cc8f6e8e1ea2213520085a69f085dc00c8e3d5
ceiara <a01074075104@gmail.com>
2022-06-23 16:41:31 +0900

    ex0623

---
+++ java_work/ex0623/src/FF.java
@@ -0,0 +1,21 @@
+
+public class FF {
+
+	public static void main(String[] args) {
+		
+		boolean _a1 = true;
+		boolean aa = false;
+		System.out.println(_a1);
+		
+		char b = 'A';
+		int c = 1231331312;
+		
+		float kk = 1.2f;
+		double qq = 1.3d;
+		
+		System.out.println(kk);
+		System.out.println(qq);
+		
+		
+	}
+}
