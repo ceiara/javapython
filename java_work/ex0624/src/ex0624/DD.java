9f4bc0db380a3a0fc308357cbcb2a81c05deb9ce
ceiara <a01074075104@gmail.com>
2022-06-24 11:57:14 +0900

    ex

--- java_work/ex0624/src/ex0624/DD.java
+++ java_work/ex0624/src/ex0624/DD.java
@@ -17,5 +17,15 @@ public class DD {
 		System.out.println("\\안녕하세요\\");
 //		AA = 20;
 		
+//		한번만
+//		git init 현재폴더를 git으로 초기화
+//		git config email.name
+//		git remote add 주소
+//		
+//		할때마다
+//		git add . 현재폴더밑에 있는 변경된 파일 올릴꺼다
+//		git commit -m
+//		git push origin main
+		
 	}
 }
---
+++ java_work/ex0624/src/ex0624/GG.java
@@ -0,0 +1,19 @@
+package ex0624;
+
+public class GG {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		
+		System.out.println(num1 > num2);
+		System.out.println(num1 < num2);
+		
+		System.out.println(num1 < num2 && num2 < num3); // 앞에가 true이면 뒤에 것 연산
+		System.out.println(num1 > num2 && num2 < num3);
+		
+		System.out.println(!true);
+		
+	}
+}
